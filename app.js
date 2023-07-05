9cdeb3ad972b1c02dfbf724d3e92b5d3dfa8b4a1
Qube24 <k.twerdochlib@gmail.com>
2023-07-05 22:09:13 +0200

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Welcome page
+// Welcome page walić sendgrid
 
 app.get("/", (req, res) => {
 	res.send('WELCOME TO THE MAIN PAGE. Path to contact is "/api/contacts"');
